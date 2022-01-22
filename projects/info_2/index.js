64e1e63934b4d232ce3274aa339830deefe8e685
Phillipe Rodriguez <philliperod@gmail.com>
2022-01-22 13:26:52 -0600

    created two components - navbar and content; navbar needs some work, but will get to that later with the other lessons

--- projects/info_2/index.js
+++ projects/info_2/index.js
@@ -1,13 +1,13 @@
-// second assignment
+// // second assignment
 function Navbar() {
   return (
     <nav className='navbar navbar-expand-lg navbar-light bg-light'>
-      <a href='#' className='navbar-brand'>
+      <a className='navbar-brand' href='#'>
         Navbar
       </a>
       <button
-        type='button'
         className='navbar-toggler'
+        type='button'
         data-toggle='collapse'
         data-target='#navbarSupportedContent'
         aria-controls='navbarSupportedContent'
@@ -16,6 +16,7 @@ function Navbar() {
       >
         <span className='navbar-toggler-icon'></span>
       </button>
+
       <div className='collapse navbar-collapse' id='navbarSupportedContent'>
         <ul className='navbar-nav mr-auto'>
           <li className='nav-item active'>
@@ -24,13 +25,15 @@ function Navbar() {
             </a>
           </li>
           <li className='nav-item'>
-            <a className='nav-link' href='#'></a>
+            <a className='nav-link' href='#'>
+              Link
+            </a>
           </li>
           <li className='nav-item dropdown'>
             <a
               className='nav-link dropdown-toggle'
-              id='navbarDropdown'
               href='#'
+              id='navbarDropdown'
               role='button'
               data-toggle='dropdown'
               aria-haspopup='true'
@@ -38,7 +41,7 @@ function Navbar() {
             >
               Dropdown
             </a>
-            <div className='dropdown-menu' aria-labelledy='navbarDropdown'>
+            <div className='dropdown-menu' aria-labelledby='navbarDropdown'>
               <a className='dropdown-item' href='#'>
                 Action
               </a>
@@ -46,13 +49,13 @@ function Navbar() {
                 Another action
               </a>
               <div className='dropdown-divider'></div>
-              <a id='' className='dropdown-item'>
+              <a className='dropdown-item' href='#'>
                 Something else here
               </a>
             </div>
           </li>
           <li className='nav-item'>
-            <a className='nav-link' href='#'>
+            <a className='nav-link disabled' href='#'>
               Disabled
             </a>
           </li>
@@ -73,9 +76,19 @@ function Navbar() {
   );
 }
 
+function MainContent() {
+  return (
+    <div>
+      <h1>This is your header</h1>
+      <p>This is the content. That's it. Not much...</p>
+    </div>
+  );
+}
+
 ReactDOM.render(
   <div>
     <Navbar />
+    <MainContent />
   </div>,
   document.getElementById('root')
 );
