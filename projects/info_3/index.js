920716dab87ff3339b9d28a4edd421d77eed8626
Phillipe Rodriguez <philliperod@gmail.com>
2022-01-29 14:14:12 -0600

    added some dependencies but still facing error with babel; ran the same code on an online editor and it works just fine; need to figure out what I'm doing wrong with vs code

--- projects/info_3/index.js
+++ projects/info_3/index.js
@@ -1,6 +1,11 @@
-// const h1 = document.createElement("h1")
-// h1.textContent = "Hello world"
-// h1.className = "header"
-// console.log(h1)
+'use strict';
 
-ReactDOM.render(<h1>This is JSX</h1>, document.getElementById('root'));
+const h1 = document.createElement('h1');
+h1.textContent = 'Hello world';
+h1.className = 'header';
+console.log(h1);
+
+const element = <h1 className='header'>This is JSX</h1>;
+console.log(element);
+
+ReactDOM.render(element, document.getElementById('root'));
