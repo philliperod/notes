2d4aa7ab2900b29a6d063c3444a458eb4a046985
Phillipe Rodriguez <philliperod@gmail.com>
2022-01-29 18:30:27 -0600

    added some more notes and pictures; finally got the practice code to work

--- projects/info_3/index.js
+++ projects/info_3/index.js
@@ -1,11 +1,13 @@
-'use strict';
+// const h1 = document.createElement('h1');
+// h1.textContent = 'Hello world';
+// h1.className = 'header';
+// console.log(h1);
 
-const h1 = document.createElement('h1');
-h1.textContent = 'Hello world';
-h1.className = 'header';
-console.log(h1);
-
-const element = <h1 className='header'>This is JSX</h1>;
-console.log(element);
+const element = (
+  <div>
+    <h1 className='header'>This is React</h1>
+    <p>We're using JSX</p>
+  </div>
+);
 
 ReactDOM.render(element, document.getElementById('root'));
