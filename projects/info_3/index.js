c24bf7d320ad75429fd88af709dbade9dfc35e17
Phillipe Rodriguez <philliperod@gmail.com>
2022-01-30 12:25:02 -0600

    created a basic navbar with JSX

--- projects/info_3/index.js
+++ projects/info_3/index.js
@@ -1,7 +1,8 @@
-// const h1 = document.createElement('h1');
-// h1.textContent = 'Hello world';
-// h1.className = 'header';
-// console.log(h1);
+/*
+const h1 = document.createElement('h1');
+h1.textContent = 'Hello world';
+h1.className = 'header';
+console.log(h1);
 
 const element = (
   <div>
@@ -9,6 +10,30 @@ const element = (
   </div>
 );
 
-console.log(element);
-
 ReactDOM.render(element, document.getElementById('root'));
+*/
+
+/*
+Challenge: 
+
+Create a navbar in JSX:
+    - Use the semantic `nav` element as the parent wrapper
+    - Have an h1 element with the brand name of your "website"
+    - Insert an unordered list for the other nav elements
+        - Inside the `ul`, have three `li`s for "Pricing",
+        "About", and "Contact"
+    - Don't worry about styling yet - it'll just be plain-looking HTML for now
+*/
+
+const navbar = (
+  <nav>
+    <h1>Scrimba Womba</h1>
+    <ul>
+      <li>Pricing</li>
+      <li>About</li>
+      <li>Contact</li>
+    </ul>
+  </nav>
+);
+
+ReactDOM.render(navbar, document.getElementById('root'));
