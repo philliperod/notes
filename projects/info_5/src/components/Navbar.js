9ceb930b6007105603110b729820fb84bd96220f
Phillipe Rodriguez <philliperod@gmail.com>
2022-02-21 18:58:17 -0600

    added styling to the navbar; got the react spinning logo going too

--- projects/info_5/src/components/Navbar.js
+++ projects/info_5/src/components/Navbar.js
@@ -1,5 +1,19 @@
 import React from 'react';
+import logo from './logo.svg';
+import './Navbar.css';
 
 export default function Navbar() {
-  return <h1>Navbar goes here</h1>;
+  return (
+    <div className='navbar'>
+      <ul className='nav-items'>
+        <li className='nav-item'>
+          <img className='logo' src={logo} alt='logo' />
+          <h3>ReactFacts</h3>
+        </li>
+        <li className='nav-item'>
+          <h4>React Course - Project 1</h4>
+        </li>
+      </ul>
+    </div>
+  );
 }
