3e8208935ec0167a4fad01dc8ce0415b75f88063
Phillipe Rodriguez <philliperod@gmail.com>
2022-02-21 19:17:32 -0600

    refactor index.html with more semantic tags; reduced Navbar.css size

--- projects/info_5/src/components/Navbar.js
+++ projects/info_5/src/components/Navbar.js
@@ -4,16 +4,10 @@ import './Navbar.css';
 
 export default function Navbar() {
   return (
-    <div className='navbar'>
-      <ul className='nav-items'>
-        <li className='nav-item'>
-          <img className='logo' src={logo} alt='logo' />
-          <h3>ReactFacts</h3>
-        </li>
-        <li className='nav-item'>
-          <h4>React Course - Project 1</h4>
-        </li>
-      </ul>
-    </div>
+    <nav>
+      <img className='logo' src={logo} alt='logo' />
+      <h3>ReactFacts</h3>
+      <h4>React Course - Project 1</h4>
+    </nav>
   );
 }
