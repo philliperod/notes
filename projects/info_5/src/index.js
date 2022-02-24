c31ac5d72e985c5d25cd6628a758940392fd2d1d
Phillipe Rodriguez <philliperod@gmail.com>
2022-02-23 19:26:07 -0600

    new challenge: adding bck-img to the right side of the page

--- projects/info_5/src/index.js
+++ projects/info_5/src/index.js
@@ -3,14 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-/**
-Challenge: Build the main section!
+/* 
+Challenge: Place the gray react logo in the background
 
-Skip 2 aspects of the design for now:
-1. The colored bullets in the list
-2. The larger React logo on the side
+Don't use an `img` element, but rather set it as the
+`background-image` of the `main` element.
 
-Those will be separate challenges coming up.
+Hint: you'll need to use the following properties:
+- background-image
+- background-repeat
+- background-position
+
+(Or the shorthand `background` property with values for all
+those other properties)
 */
 
 ReactDOM.render(
